9255f958e4a58b6bb6270e6413c2d001fe1f63eb
Jose Luis Rivero <jrivero@osrfoundation.org>
2015-09-14 17:42:33 +0200

    New packages names

--- trusty/debian/rename_to_prerelease.bash
+++ trusty/debian/rename_to_prerelease.bash
@@ -6,8 +6,8 @@ fi
 for i in *${1}*; do
     name=${i/.*}
     extension=${i#*.}
-    new_file=${name/6/7}.${extension}
-    #new_file=${name}-prerelease.${extension}
+    #new_file=${name/6/7}.${extension}
+    new_file=${name}-haptix.${extension}
     echo "$i -> ${new_file}"
-    mv $i ${new_file}
+    hg mv $i ${new_file}
 done
